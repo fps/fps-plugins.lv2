0ef3aa0b1fdb93c714595f81543717d6484cfc6b
Florian Paul Schmidt <mista.tapas@gmx.net>
2023-05-06 08:31:25 +0200

    seems to work allright

--- test_eq_match.cc
+++ test_eq_match.cc
@@ -5,6 +5,18 @@ int main ()
 {
   {
     eq_match match (1024, 48000);
+    match.reset_buffer1 ();
+    match.reset_buffer2 ();
+
+    std::vector<float> buffer (100);
+
+    for (size_t index = 0; index < 1000; ++index)
+    {
+      match.add_frames_to_buffer1 (&buffer[0], 100);
+      match.add_frames_to_buffer2 (&buffer[0], 100);
+    }
+
+    match.calculate_response ();
   }
   fftwf_cleanup ();
 }
