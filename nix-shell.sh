aecd458d3ab9ca482437578923fc7847168dead6
Florian Paul Schmidt <mista.tapas@gmx.net>
2023-05-06 08:31:50 +0200

    add some deps

--- nix-shell.sh
+++ nix-shell.sh
@@ -1,1 +1,1 @@
-nix-shell -p lilv python3 valgrind python3Packages.jinja2 lv2 fftwFloat zita-convolver
+nix-shell -p lilv python3 valgrind python3Packages.jinja2 lv2 fftwFloat zita-convolver libsndfile libsamplerate
