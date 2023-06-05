b19c9795568925d03b94c3d234e586e836359289
Florian Paul Schmidt <mista.tapas@gmx.net>
2023-06-05 14:27:26 +0200

    aha!

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-DEBUG=1 PATH=../lv2-ttl2c/:$PATH LD_LIBRARY_PATH=$(dirname $(which lv2ls))/../lib make -j4 test
+PATH=../lv2-ttl2c/:$PATH LD_LIBRARY_PATH=$(dirname $(which lv2ls))/../lib make -j4 test
